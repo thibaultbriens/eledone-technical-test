1256f751d147e7325cc3b02bfa88fca9bc750ae0
thibault.briens <thibault.briens@epita.fr>
2025-05-12 14:44:51 +0200

    feat: subject + backend working without any optimizations

--- backend/without_django.py
+++ backend/without_django.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep
 
 wasteCollected = 0
 wasteNumber = 0
@@ -23,7 +24,7 @@ def start(n, m, basePosArg):
     
     wasteNumber = m
     agentPosList = randList(n, thirdArg=True)
-    wastePosList = randList(n)
+    wastePosList = randList(m)
     basePos = basePosArg
 
     game([])
@@ -49,9 +50,13 @@ def game(knownWastePosList):
     global wasteNumber, wastePosList, agentPosList, basePos, wasteCollected
 
     newKnownWastePosList = nextTurn(knownWastePosList)
+    displayBoard()
+    sleep(1)
 
     if wasteCollected < wasteNumber:
         game(newKnownWastePosList)
+    else:
+        print("Game Over")
 
 
 '''
@@ -60,11 +65,135 @@ This is the main function !
 This function will move agents.
 @param knownWastePosList a list of all the know wastes position
 '''
-def nextTurn(knownWastePosList):
+def nextTurn(knownWastePosList : list):
     global wasteNumber, wastePosList, agentPosList, basePos, wasteCollected
 
-    wasteCollected = wasteNumber
-    return []
+    # Move agents
+    for index, (i, j, w) in enumerate(agentPosList):
+        # Drop waste if agent is at base
+        if (i, j) == basePos and w:
+            agentPosList[index] = (i, j, False)
+            wasteCollected += 1
+
+        # If is on waste, collect it
+        if (i, j) in knownWastePosList and not w:
+            agentPosList[index] = (i, j, True)
+            knownWastePosList.remove((i, j))
+            wastePosList.remove((i, j))
+        else :
+            # Move to next pos
+            nextpos = basePos
+            if not w:
+                nextpos = closestWaste(knownWastePosList, (i, j))
+
+            d = distanceToPos((i, j), nextpos)
+            if (d[0] > 0 and not any((i + 1, j) == (x, y) for x, y, _ in agentPosList)):
+                agentPosList[index] = (i + 1, j, w)
+            elif (d[0] < 0 and not any((i - 1, j) == (x, y) for x, y, _ in agentPosList)):
+                agentPosList[index] = (i - 1, j, w)
+            elif (d[1] > 0 and not any((i, j + 1) == (x, y) for x, y, _ in agentPosList)):
+                agentPosList[index] = (i, j + 1, w)
+            elif (d[1] < 0 and not any((i, j - 1) == (x, y) for x, y, _ in agentPosList)):
+                agentPosList[index] = (i, j - 1, w)
+
+            # Update the known waste pos list
+            for di in range(-5, 6):
+                for dj in range(-5, 6):
+                    if (i + di, j + dj) in wastePosList and (i + di, j + dj) not in knownWastePosList:
+                        knownWastePosList.append((i + di, j + dj))
+        
+
+    return knownWastePosList
+
+'''
+Return the distance between two positions
+@return a tuple (x, y) with x the distance in x and y the distance in y
+'''
+def distanceToPos(pos1, pos2):
+    return (pos2[0] - pos1[0], pos2[1] - pos1[1])
+
+
+'''
+Return the position of the closest waste
+@param wastePosList a list of all the known waste positions
+@param pos the position of the agent
+
+@return the position of the closest waste or a strategic exploration position if there is no waste
+'''
+def closestWaste(knownWastePosList, pos):
+    if len(knownWastePosList) == 0:
+        # Strategic exploration instead of pure randomness
+        x, y = pos
+        
+        # Create sectors and assign a target position based on current position
+        if x < 10:
+            target_x = 25 
+        elif x > 21:
+            target_x = 6
+        else:
+            target_x = x
+            
+        if y < 10:
+            target_y = 25 
+        elif y > 21:
+            target_y = 6
+        else:
+            target_y = y
+        
+        # Randomness to avoid same paths for every agents
+        target_x += random.randint(-3, 3)
+        target_y += random.randint(-3, 3)
+        
+        # Ensure within bounds
+        target_x = max(0, min(31, target_x))
+        target_y = max(0, min(31, target_y))
+        
+        return (target_x, target_y)
+    
+    closestWaste = knownWastePosList[0]
+    minDistance = distanceToPos(pos, closestWaste)
+    for waste in knownWastePosList:
+        d = distanceToPos(pos, waste)
+        if (abs(d[0]) + abs(d[1])) < (abs(minDistance[0]) + abs(minDistance[1])):
+            closestWaste = waste
+            minDistance = d
+
+    return closestWaste
+
+'''
+Display the board with the agents and the wastes
+'''
+def displayBoard():
+    global wasteNumber, wastePosList, agentPosList, basePos, wasteCollected
+    
+    # Create an empty 32x32 board
+    board = [[' ' for _ in range(32)] for _ in range(32)]
+    
+    # Place wastes on the board
+    for waste_x, waste_y in wastePosList:
+        board[waste_x][waste_y] = '.'
+    
+    # Place agents on the board
+    for agent_x, agent_y, _ in agentPosList:
+        board[agent_x][agent_y] = 'X'
+    
+    # Place base on the board
+    base_x, base_y = basePos
+    board[base_x][base_y] = '$'
+    
+    # Print the board
+    print("\n" + "-" * 66)
+    for row in board:
+        print("|", end=' ')
+        for cell in row:
+            print(cell, end=' ')
+        print("|")
+    print("-" * 66)
+    
+    # Display waste collection status
+    print(f"Waste collected: {wasteCollected}/{wasteNumber}")
+    print(f"Agents: {agentPosList}")
+    print("-" * 66)
 
 
 # Main
